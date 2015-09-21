6a75c175a9977de7937e570030501dda9b8c8a3a
taoka <mrp1q7z@gmail.com>
2015-09-22 08:16:21 +0900

    authorモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921221532) do
+ActiveRecord::Schema.define(version: 20150921231423) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
