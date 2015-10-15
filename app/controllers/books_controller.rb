b0e7e0247136038022a1a018a4626b2a890a2c5c
taoka <mrp1q7z@gmail.com>
2015-10-16 06:30:37 +0900

    本の検索にページング機能を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
   def search
     @keywd = params[:keywd]
-    @res = AmazonBook.find(@keywd)
+    Amazon::Ecs.debug = true
+    @res = AmazonBook.find(@keywd, params[:page])
+    # dummy = []
+    # (@res.total_results - @res.item_page).times { dummy << "" }
+    # @res.concat(dummy)
+    # @res = Kaminari.paginate_array(@res).page(params[:page]).per(@res.item_page)
   end
 end
--- app/models/amazon_book.rb
+++ app/models/amazon_book.rb
@@ -3,9 +3,9 @@ class AmazonBook
 
   attr_accessor :isbn, :asin, :title, :image_url, :authors, :publish, :publication_date, :page_url
 
-  def self.find(keyword)
+  def self.find(keyword, page)
     res = Amazon::Ecs.item_search(keyword,
-      { search_index: 'Books', response_group: 'Medium', country: 'jp' })
+      { search_index: 'Books', response_group: 'Medium', country: 'jp', item_page: page || 1 })
 
     return nil unless res
 
@@ -24,6 +24,29 @@ class AmazonBook
       book.isbn = element.get('ISBN')
       results << book
     end
+    # AmazonECS item_page に指定できるのは1〜10まで
+    # それ以上の商品を見せる場合は、MoreSearchResultsUrl を使う必要がある！
+    Rails.logger.debug "taoka99: total_pages=#{res.total_pages}"
+    add_dummy_array(results, page, res.total_results, res.items.size)
+  end
+
+  private
+
+  def self.add_dummy_array(results, page, total_results, page_size)
+    if page == 1
+      dummy = (1..(total_results - page_size)).to_a
+      results.concat(dummy)
+    else
+      dummy = (1..(page_size * (page.to_i - 1))).to_a
+      results = dummy.concat(results)
+      dummy = (100..(total_results - (page_size * (page.to_i - 1)))).to_a
+      results.concat(dummy)
+    end
+
+    Rails.logger.debug "taoka0: page=#{page}, total_results=#{total_results}, page_size=#{page_size}"
+    Rails.logger.debug "taoka1:" + results.inspect
+    results = Kaminari.paginate_array(results).page(page).per(page_size)
+    Rails.logger.debug "taoka2:" + results.inspect
     results
   end
 end
